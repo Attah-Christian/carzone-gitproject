6f8b028dc79fc2c21cf7680ab5a838f72fb528ca
Attah-Christian <112478087+Attah-Christian@users.noreply.github.com>
2023-01-07 13:28:50 +0100

    completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +80,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carzone_db',
         'USER': 'postgres',
-        'PASSWORD': 'Forgotten101',
+        'PASSWORD': 'patonly',
         'HOST': 'localhost',
+        'PORT': '1927',
     }
 }
 
@@ -122,9 +123,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIR = [
-os.path.join(BASE_DIR, 'carzone/static'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static'),
 ]
+
+#media settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
